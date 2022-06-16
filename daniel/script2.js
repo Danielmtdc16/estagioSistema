e53882070e215d68ac607379e67b53e7fa5d07e8
Danielmtdc16 <danieltdc16@gmail.com>
2022-06-16 10:13:07 -0300

    Primeiro commit

---
+++ daniel/script2.js
@@ -0,0 +1,196 @@
+function CommandButton1_Click(){
+    let Q, L, NR, e, Di, f, hf, RgHid, Tempa, Kelv, Lgu, Uc, u, Fct, mespa, QT, Dint;
+
+    Q = Val(TextBox5.value);
+    L = Val(TextBox7.value);
+    e = Val(ComboBox3.value);
+    Di = Val(ComboBox1.value);
+    Tempa = Val(TextBox10.value);
+
+//viscosidade cinemática da água na lateral
+    
+    Kelv = Tempa + 273.16;
+    Lgu = (-11.73) + (1828 / Kelv) + (0.01966 * Kelv) + (-0.00001466 * (Kelv ^ 2));
+    u = ((10 ^ Lgu) / 100).toFixed(5);
+    Uc = u * 1000;
+    Label14 = "  Viscosidade dinâmica:  " & Uc & " x 10-³ N.s/m²";
+    
+//massa específica e fator de correção da temperatura FCt na lateral
+     
+    Fct = ((Tempa - 3.983035) ^ 2) * (Tempa + 301.797) / (522528.9 * (Tempa + 69.34881));
+    mespa = (1000 * (1 - Fct)).toFixed(2);
+    Label15 = "  Massa específica:  " & mespa & " kg/m³";
+ 
+//Velocidade da água na tubulação:
+
+ //Para vazão em m³/h e diâmetro em mm
+    
+    if (OptionButton1.Value == true){
+        TextBox9.Text = ((353.67765 * Q / Di ^ 2)).toFixed(2);
+    }
+    
+  //Else
+  
+//Para vazão em L/h e diâmetro em mm
+
+    else if (OptionButton2.Value == true){
+        TextBox9.value = (Q / (2.8274 * Di ^ 2)).toFixed(2);
+    }
+ 
+//Nº de Ryenolds
+  
+    TextBox1.value = (mespa * TextBox9.Text * Di / Uc).toFixed(1);
+   
+//Rugosidade hidráulica
+   
+    TextBox8.value = ((TextBox1.Text ^ 0.9) * e / Di).toFixed(2);
+      
+    if (TextBox1.value < 2000 && OptionButton1.value == true){
+        f = 64 / TextBox1.value;
+        TextBox4.value = f.toFixed(4);
+        Label12 = "Hagen-Poiseuille";
+        TextBox6.value = (11.536 * 10 ^ 8 * u / mespa * Q * L / (Di ^ 4)).toFixed(2);
+        Label22 = "Fluxo Laminar";
+        Label9 = "";
+    
+    } else if (TextBox1.value < 2000 && OptionButton2.value == true){
+        f = 64 / TextBox1.value;
+        TextBox4.Text = f.toFixed(4);
+        Label12 = "Hagen-Poiseuille";
+        TextBox6.value = (11.536 * 10 ^ 5 * u / mespa * Q * L / (Di ^ 4)).toFixed(3);
+        Label22 = "Fluxo Laminar";
+        Label9 = "";
+        
+    } else {
+        Er = 0.001
+        oldf = 1
+        deltaf = oldf
+   
+        while (Math.abs(deltaf / oldf) >= Er){
+
+            Newf = 1 / (-2 * Log(e / (3.7 * Di) + 2.51 / (TextBox1.value * Sqr(oldf))) * 0.434294482) ^ 2;
+            deltaf = Newf - oldf;
+            oldf = Newf;
+        }
+
+        f = oldf;
+        TextBox4.value = f.toFixed(4);
+                                    
+        //Cálculo do fator f e da perda de carga utilizando a equaçâo de Blasius
+        
+        //Para vazão em m³/h e diâmetro em mm
+        
+        if ((TextBox1.value >= 3000) && (TextBox1.value <= 100000) && (OptionButton1.value = true) && (TextBox8.value <= 31)){
+                
+            f = 0.316 / (TextBox1.value) ^ 0.25
+            TextBox4.value = f.toFixed(4);
+            TextBox6.value = (2.6125 * 10 ^ 6 * (u / mespa) ^ 0.25 * (Q ^ 1.75) * L / (Di ^ 4.75)).toFixed(4);
+            Label12 = "Blasius";
+            Label22 = "Fluxo Turbulento";
+            Label9 = "&  Tubo Hidraulicamente Liso";
+                    
+        }
+        
+        if ((TextBox1.value >= 3000) && (TextBox1.value <= 100000) && (OptionButton1.value == true) && (TextBox8.value > 31 && TextBox8.value < 448)){
+            TextBox6.value = (6.376 * 10 ^ 6 * TextBox4.value * (Q ^ 2) * L / (Di ^ 5)).toFixed(2);
+            Label12 = "Colebrook";
+            Label22 = "Fluxo Turbulento";
+            Label9 = "&  Tubo Hidraulicamente misto";
+        }
+        
+        //Para vazão em L/h e diâmetro em mm
+        
+        if ((TextBox1.value >= 3000) && (TextBox1.value <= 100000) && (OptionButton2.value == true) && (TextBox8.value <= 31)){
+            f = 0.316 / (TextBox1.value) ^ 0.25;
+            TextBox4.value = f.toFixed(4);
+            TextBox6.value = (14.69 * (u / mespa) ^ 0.25 * (Q ^ 1.75) * L / (Di ^ 4.75)).toFixed(4);
+            Label12 = "Blasius";
+            Label22 = "Fluxo Turbulento";
+            Label9 = "&  Tubo Hidraulicamente Liso";
+        }
+            
+        //If TextBox8.Text <= 31 Then
+            
+            //Label9 = "&  Tubo Hidraulicamente Liso"
+            
+        //End If
+        
+        //Cálculo do fator f pela equação de Colebrook utilizando a equaçâo de Darcy
+                                
+        //Para vazão em m³/h e diâmetro em mm
+        
+        if ((TextBox1.value > 100000) && (OptionButton1.value == true) && (TextBox8.value > 31) && (TextBox8.value < 448)){
+
+            TextBox6.value = (6.376 * 10 ^ 6 * TextBox4.value * (Q ^ 2) * L / (Di ^ 5)).toFixed(2);
+            Label12 = "Colebrook";
+            Label22 = "Fluxo Turbulento";
+            Label9 = "&  Tubo Hidraulicamente misto";
+                    
+        }            
+        //Para vazão em L/h e diâmetro em mm
+        
+        if ((TextBox1.value > 100000) && (OptionButton2.value == true) && (TextBox8.value) > 31  && (TextBox8.value < 448)){
+            TextBox6.value = (6.376 * TextBox4.Text * (Q ^ 2) * L / (Di ^ 5)).toFixed(2);
+            Label12 = "Colebrook";
+            Label22 = "Fluxo Turbulento";
+            Label9 = "&  Tubo Hidraulicamente misto";
+                    
+        }
+            
+        //If TextBox8.Text > 31 And TextBox8.Text < 448 Then
+            
+            //Label9 = "&  Tubo Hidraulicamente misto"
+            
+        //End If
+            
+        if (TextBox8.value > 448){
+            Label9 = "&  Tubo Hidraulicamente rugoso";
+        }
+              
+    }
+ActiveWorkbook.Save
+}
+
+function Frame2_Click(){
+
+}
+
+function OptionButton1_Click(){
+
+    if (OptionButton1.value == true){
+        TextBox5.SetFocus;
+    }
+}
+
+function OptionButton2_Click(){
+
+    if (OptionButton2.value == true){
+        TextBox5.SetFocus;
+    }
+}
+
+function TextBox9_Change(){
+
+}
+
+function UserForm_Initialize(){
+    //Diâmetro interno de tubos de PEBD e PVC
+    ComboBox1.innerHTML = "5.3 <br>";
+    ComboBox1.innerHTML = "13 <br>";
+    ComboBox1.innerHTML = "13.6 <br>";
+    ComboBox1.innerHTML = "16 <br>";
+    ComboBox1.innerHTML = "20.6 <br>";
+    ComboBox1.innerHTML = "26.9 <br>";
+    ComboBox1.innerHTML = "35.7 <br>";
+    ComboBox1.innerHTML = "48.1 <br>";
+    ComboBox1.innerHTML = "72.5 <br>";
+    ComboBox1.innerHTML = "97.6"; //acrescentar novos diâmetros
+    //Rugosidade absoluta de tubos de PEBD e PVC
+    ComboBox3.innerHTML = "0.0015 <br>";
+    ComboBox3.innerHTML = "0.08 <br>";
+    ComboBox3.innerHTML = "0.15";
+    //Material dos tubos
+    ComboBox4.innerHTML = "PEBD <br>";
+    ComboBox4.innerHTML = "PVC <br>";
+    ComboBox4.innerHTML = "AZDº";
+}
---
+++ madson/scriptestagio.js
@@ -0,0 +1,548 @@
+
+
+function CheckBox1_Click(){
+
+    if(CheckBox1.value == true){
+
+    CheckBox2.Enabled = false;
+    Label56.visible = false
+    TextBox39.visible = false
+    TextBox18.visible = true
+    TextBox40.Enabled = true
+    CheckBox2.value = false
+    CheckBox3.value = false
+    }
+ 
+    else{
+    CheckBox2.Enabled = true
+    Label56.visible = false
+    TextBox39.visible = false
+    TextBox18.visible = true
+    TextBox40.Enabled = false
+    }
+
+}
+
+function CheckBox2_Click(){
+
+    if(CheckBox2.value == true){
+
+    CheckBox1.Enabled = false
+    CheckBox3.value = false
+    Label56.visible = false
+    TextBox39.visible = false
+    TextBox18.visible = true
+    TextBox40.Enabled = false
+    }
+
+    else{
+
+        CheckBox1.Enabled = true
+        Label56.visible = false
+        TextBox39.visible = false
+        TextBox18.visible = true
+        TextBox40.Enabled = true
+    }
+
+}
+
+
+function CheckBox3_Click(){
+
+    if(CheckBox3.value == true){
+
+    CheckBox2.value = false
+    CheckBox1.value = false
+    Label56.visible = true
+    TextBox39.visible = true
+    TextBox18.visible = false
+    }
+    
+    else{
+
+    Label56.visible = false
+    TextBox39.visible = false
+    TextBox18.visible = true
+    }
+
+}
+
+function CommandButton2_Click(){
+
+    let Qsuc, Lsuc, Dsuc, Rgsuc, Ke, Kvg, kc, Kvpc, Alt, Temp, NPSH;
+    let Hfls, Hfss, Patm, Pv, HfTs, Zscritica, Pes, Vs, Zsmax, NPSHd, Zspre;
+    let Kelv, tempa, Lgu, u, Uc, Fct, ZsAfog, NPSHdreal, mespa, Zsmod;
+
+//Parâmetros para determinar a Pressão de vapo d'água
+
+    let logPv, PvHg, Pvm;
+
+    let objeto;
+
+    let decisao;
+
+    Qsuc = TextBox1.value;
+    Lsuc = TextBox2.value;
+    Dsuc = ComboBox7.value;
+    Rgsuc = ComboBox1.value;
+    Ke = ComboBox2.value;
+    Kvg = ComboBox3.value;
+    kc = ComboBox4.value;
+    Kvpc = ComboBox5.value;
+    Alt = TextBox4.value;
+    tempa = ComboBox6.value;
+    ZsAfog = TextBox39.value;
+    NPSH = TextBox6.value;
+    Zspre = TextBox40.value;
+
+    if ((TextBox1.value == "") || (TextBox2.value == "") || (TextBox4.value == "") || (TextBox6.value == "") || (ComboBox6.value == "")) {
+    
+        alert("Preenchimento do campo obrigatório.");
+        
+    } else {
+    
+    //'viscosidade cinemática da água na sucção
+        
+        Kelv = tempa + 273.16;
+        Lgu = (-11.73) + (1828 / Kelv) + (0.01966 * Kelv) + (-0.00001466 * (Kelv ^ 2));
+        u = ((10 ^ Lgu) / 100).toFixed(5);
+        Uc = u * 1000;
+        Label54 = ("Viscosidade dinâmica:  " + Uc + " x 10-³ N.s/m²");
+        
+    //'massa específica e fator de correção da temperatura FCt na sucção
+        
+        Fct = ((tempa - 3.983035) ^ 2) * (tempa + 301.797) / (522528.9 * (tempa + 69.34881));
+        mespa = (1000 * (1 - Fct)).toFixed(2);
+        Label55 = ("Massa específica:  " + mespa + " kg/m³");
+    
+    //'Peso específico da água
+
+    TextBox9.value = (mespa * 9.81).toFixed(2);
+    Pes = TextBox9.value;
+    
+    //'Pressão de vapor da água em função da temperatura
+    
+    logPv = 8.0701 - 1730.6 / (tempa + 233.4);
+    PvHg = 10 ^ logPv;
+    Pvm = PvHg * 10.33 / 760;
+    TextBox17.value = (Pvm).toFixed(2);
+    
+    //'Velocidade da água na tubulação de sucção
+
+    TextBox12.value = (353.6775 * Qsuc / (Dsuc ^ 2)).toFixed(2);
+        
+    //'Número de Reynolds na sucção
+
+    TextBox10.value = (mespa * TextBox12.value * (Dsuc / 1000) / u).toFixed(1);
+
+    //'Derterminação do fator de atrito e do regime de escoamento na sucção
+
+        if (TextBox10.value <= 2300) {
+        
+            TextBox15.value = (64 / TextBox10.value).toFixed(2);
+            Label27 = "Escoamento Laminar"
+            //'Label27.ForeColor = &H4000&
+                    
+        }else{
+            er = 0.001;
+            oldf = 1;
+            deltaf = oldf;
+        while( Math.abs(deltaf / oldf) >= er){
+            Newf = 1 / (-2 * Math.log(Rgsuc / (3.7 * Dsuc) + 2.51 / (TextBox10.value * Math.sqrt(oldf))) * 0.434294482) ^ 2;
+            deltaf = Newf - oldf;
+            oldf = Newf;
+        }
+            F = oldf;
+            TextBox11.value = (F).toFixed(4);
+        }
+        
+        if (TextBox10.value >= 4000){
+            
+            Label27.value = "Escoamento Turbulento";
+           // 'Label27.ForeColor = &H4000&
+        }
+                
+    //'Perda de carga distribuída na sucção
+
+    TextBox13.value = (6.376 * (10 ^ 6) * TextBox11.value * (Qsuc ^ 2) * Lsuc / Dsuc ^ 5).toFixed(2);
+    Hfls = Math.abs(TextBox13.value) 
+    //era um CDbl
+        
+    //'Perda de carga nas singularidades da sucção
+
+    TextBox14.value = ((Ke + Kvg + kc + Kvpc) * (TextBox12.Text ^ 2) / 19.62).toFixed(2);
+    Hfss = Math.abs(TextBox14.value)
+    //era um CDbl
+
+    //'Perda de carga total na sucção
+
+    TextBox15.value = (Hfls + Hfss).toFixed(2);
+    HfTs = Math.abs(TextBox15.value)
+    //era um CDbl
+
+    //'Pressão atmosférica local
+
+    TextBox16.value = (10.33 * ((293 - 0.0065 * Alt) / 293) ^ 5.26).toFixed(2);
+    Patm = Math.abs(TextBox16.value)
+    //era um CDbl
+
+    //'Altura estática de sucção positiva
+
+    if (CheckBox2.value == true){
+    
+    //'Altura estática de sucção
+
+    TextBox18.value = (Patm - (NPSH + Pvm + HfTs)).toFixed(2)
+    Zscritica = Math.abs(TextBox18.value)
+    //era um CDbl
+    
+    //'Altura estática de sucção real
+    
+    NPSHdreal = (1.15 * NPSH).toFixed(2)
+    Zsmax = (Patm - (NPSHdreal + HfTs + Pvm)).toFixed(2)
+
+    Label29.visible = true;
+    Label29 = "Altura estática de sucção máxima = " + Zsmax + " m acima do nível da água no reservatório com NPSHdisp = " + NPSHdreal + "  m";
+    //Label29.ForeColor = &H4000&
+    
+    //'NPSH disponível para sucção positiva
+
+    TextBox19.value = (Patm - (Zscritica + Pvm + HfTs)).toFixed(2)
+    Label23.Caption = "NPSH disponível limiar (m)"
+    NPSHd = Math.abs(TextBox19.Text)
+    //era um CDbl
+    
+    //'Pressão na secção de entrada da bomba na sucção positiva
+    
+    //Em KPa
+    
+    TextBox7.value = (-TextBox9.value * (Zsreal + ((TextBox12.value ^ 2) / 19.62) + HfTs) / 1000).toFixed(2)
+
+    //Em mca
+    
+    TextBox8.value = (-(Zsreal + ((TextBox12.value ^ 2) / 19.62) + HfTs)).toFixed(2)
+        
+    }
+
+    //'Para tubulação na sucção negativa "Afogada"
+
+    if (CheckBox3.value == true){
+    
+    //'NPSH disponível para sucção negativa (Afogada)
+
+    TextBox19.value = (Patm + ZsAfog - (Pvm + HfTs)).toFixed(2)
+    Label23.Caption = "NPSH disponível (m)"
+    //'Label29.visible = false;
+    NPSHd = Math.abs(TextBox19.value)
+    
+    //'Pressão na secção de entrada da bomba
+    
+    //'Em mca
+    
+    TextBox8.value = (ZsAfog - HfTs - (TextBox12.value ^ 2 / 19.62)).toFixed(2)
+    
+    //'Em KPa
+        
+        TextBox7.value = (Pes * TextBox8.value / 1000).toFixed(2)
+        
+    Label29 = "Altura estática de sucção mínima de: " + ZsAfog + " m abaixo do nível inferior da água no reservatório.";
+    //Label29.ForeColor = &H4000&
+    
+    }
+
+    if (CheckBox1.value == true){
+        
+        TextBox19.value = (Patm - (Zspre + Pvm + HfTs)).toFixed(2)
+        Label23.Caption = "NPSH disponível (m)"
+        Label29.visible = false;
+        
+    //'Pressão na secção de entrada da bomba na sucção positiva pré-definida
+
+    //'Em KPa
+    
+    TextBox7.value = (-TextBox9.value * (Zspre + ((TextBox12.value ^ 2) / 19.62) + HfTs) / 1000).toFixed(2)
+
+    //'Em mca
+    
+    TextBox8.value = (-(Zspre + ((TextBox12.value ^ 2) / 19.62) + HfTs)).toFixed(2)
+
+    }
+
+    if ((CheckBox2.value == true) && (Zs < 0)) {
+
+    CheckBox2.value = false
+    CheckBox3.value = true
+    Label56.visible = true
+    
+    TextBox18.visible = false
+    Zsmod = (-1 * Zs).toFixed(2)
+    
+    Label29 = "Altura estática de sucção mínima de: " & Zsmod & " m abaixo do nível inferior da água no reservatório.";
+    //Label29.ForeColor = &H4000&
+    }
+
+    //'Limite de velocidade na tubulação de sucção
+
+    if (TextBox12.value > 1.5) {
+
+    alert("Velocidade da água na Tubulação de sucção acima do limite permitido. Escolha um diâmetro imdiatamente superior");
+    
+    }
+
+} // fim da funcao
+
+function CommandButton3_Click(){
+
+    let Lrec, Drec, Rgrec, Aer, d, Kvgr, Kvr, kcr, Filt, Psd, Qrec;
+    let Hftr, HfLr, HfSr, Vr, Vsuc, PesAg, PAdm, PSaida, Hb, Rend, PotB;
+    let tempa, Fct, Kelv, mespa, Lgu, u;
+
+    Qrec = TextBox1.value;
+    Lrec = TextBox20.value;
+    Drec = ComboBox11.value;
+    Rgrec = ComboBox12.value;
+    Aer = TextBox23.value;
+    d = TextBox24.value;
+    Kvgr = ComboBox8.value;
+    Kvr = ComboBox9.value;
+    kcr = ComboBox10.value;
+    Filt = TextBox25.value;
+    Psd = TextBox26.value;
+    PesAg = TextBox9.value;
+    PAdm = TextBox8.value;
+    Vsuc = TextBox12.value;
+    Rend = TextBox38.value;
+    tempa = ComboBox6.value;
+
+    //'viscosidade cinemática da água
+
+    Kelv = tempa + 273.16
+    Lgu = (-11.73) + (1828 / Kelv) + (0.01966 * Kelv) + (-0.00001466 * (Kelv ^ 2))
+    u = ((10 ^ Lgu) / 100).toFixed(5)
+    
+    
+    //'massa específica e fator de correção da temperatura FCt
+        
+        Fct = ((tempa - 3.983035) ^ 2) * (tempa + 301.797) / (522528.9 * (tempa + 69.34881))
+        mespa = (1000 * (1 - Fct)).toFixed(2)
+
+
+    if ((TextBox20.value == "") || (TextBox23.value == "") || (TextBox38.value == "") || (ComboBox11.value = "")){
+    
+        alert("Preenchimento do campo obrigatório.");
+        
+    }else{
+    
+    
+    //'Número de Reynolds no recalque
+
+    //'TextBox27.Text = Format(352610 * Qrec / Drec, "0") //ele comentou!!
+
+    TextBox27.value = (mespa * TextBox12.value * (Drec / 1000) / u).toFixed(1)
+
+    //'Derterminação do fator de atrito e do regime de escoamento no recalque
+
+        if (TextBox27.value <= 2300){
+        
+            TextBox28.value = (64 / TextBox27.value).toFixed(2)
+            //'Label27 = "Escoamento Laminar"
+            //'Label27.ForeColor = &H4000&
+                    
+        }else{
+            er = 0.001
+            oldfr = 1
+            deltafr = oldfr
+        while (Math.abs(deltafr / oldfr) >= er){
+            Newfr = 1 / (-2 * Log(Rgrec / (3.7 * Drec) + 2.51 / (TextBox27.value * Sqr(oldfr))) * 0.434294482) ^ 2
+            deltafr = Newfr - oldfr
+            oldfr = Newfr
+        }
+            Fr = oldfr
+            TextBox28.value = (Fr).toFixed(4)
+        }
+        
+
+
+
+
+
+    //--------------------------------------------------
+    //------------------------------------------------
+    // A GENTE VEIO ATE AQUI
+    //--------------------------------------------------
+    //------------------------------------------------
+
+
+
+
+        if (TextBox27.value >= 4000){
+            
+            Label27 = "Escoamento Turbulento"
+            //'Label27.ForeColor = &H4000&
+        }
+
+    //'Velocidade da água na tubulação de recalque
+
+    TextBox29.value = (353.6775 * Qrec / (Drec ^ 2)).toFixed(2);
+    Vr = Math.abs(TextBox29.value);
+
+    // era um CDBL
+    
+    if (Vr > 2){
+
+    alert("Velocidade da água na Tubulação de Recalque acima do limite permitido.")
+        
+    
+    
+    
+    //'Perda de carga distribuída no recalque
+
+    TextBox30.value = (6.3735 * TextBox28.value * ((1000 * Qrec) ^ 2) * Lrec / Drec ^ 5).toFixed(2);
+    HfLr = Math.abs(TextBox30.value);
+    
+    //'Perda de carga nas singularidades do realque
+
+    TextBox31.Text = ((Kvr + Kvgr + kcr) * (TextBox29.value ^ 2) / 19.62).toFixed(2);
+    HfSr = CDbl(TextBox31.value)
+
+    //'Perda de carga total no recalque
+
+    TextBox32.value = (HfLr + HfSr).toFixed(2)
+    Hftr = CDbl(TextBox32.value)
+    
+    //'Pressão na secção de saída da bmba
+
+    //'Em mca
+
+        TextBox34.value = (Psd + (Aer - d) + Hftr + Filt).toFixed(2);
+        PSaida = Math.abs(TextBox34.value);
+        
+    //'Em KPa
+    
+        PesAg = Math.abs(TextBox9.value) // 'Peso específico da água
+        
+        TextBox33.value = (PesAg * (Psd + (Aer - d) + Hftr) / 1000).toFixed(2);
+        
+    //'Carga da bomba
+
+    PAdm = Math.abs(TextBox8.value)
+    Vsuc = Math.abs(TextBox12.value)
+    
+    TextBox35.value = ((PSaida - PAdm) + (((Vr ^ 2) - (Vsuc ^ 2)) / 19.62) + d).toFixed(2);
+    Hb = Math.abs(TextBox35.value);
+
+    //Era um CDBL
+    
+    //'Potência da Bomba
+
+    //'Em KW
+    
+        TextBox36.value = (((PesAg * (Qrec / 3600) * Hb) / (Rend / 100) / 1000)).toFixed(2)
+        PotB = CDbl(TextBox36.value)
+    
+    //'Em CV
+    
+        TextBox37.value = (PotB / 0.7355).toFixed(2);
+        
+    }
+}
+
+function Label11_Click(){
+
+}
+
+
+//'Private Sub OptionButton1_Click()
+
+//'If OptionButton1.value = true; Then
+
+//  ' Label56.visible = false;
+//  'TextBox39.visible = false;
+// 'TextBox18.visible = true;
+// ' TextBox40.Enabled = false;
+  
+//End If
+
+
+//End Sub
+
+//'Private Sub OptionButton2_Click()
+
+//'If OptionButton2.value = true; Then
+
+//  ' Label56.visible = true;
+// ' TextBox39.visible = true;
+//'  TextBox18.visible = false;
+  
+//    End If
+//
+//'End Sub
+
+function OptionButton3_Click()
+
+if (OptionButton3.value = true){
+
+    Label40.visible = false;
+    TextBox26.visible = false;
+    TextBox26.Text = " "
+}
+
+}
+
+function OptionButton4_Click(){
+
+    if (OptionButton4.value = true)
+
+        Label40.visible = false;
+        TextBox26.visible = false;
+        TextBox26.value = " "
+    }
+
+}
+
+function OptionButton5_Click(){
+
+if (OptionButton5.value = true){
+
+    Label40.visible = true
+    TextBox26.visible = true
+  
+}
+
+}
+
+function UserForm_Initialize(){
+
+    //ComboBox7.AddItem "48.1"
+    //ComboBox7.AddItem "72.5"
+    //ComboBox7.AddItem "97.6"
+    //ComboBox7.AddItem "120"
+    //ComboBox7.AddItem "144"
+    //ComboBox6.AddItem "15"
+    //ComboBox6.AddItem "20"
+    //ComboBox6.AddItem "25"
+    //ComboBox6.AddItem "30"
+    //ComboBox1.AddItem "0.00334"
+    //ComboBox1.AddItem "0.15"
+    //ComboBox2.AddItem "0.5"
+    //ComboBox3.AddItem "0.2"
+    //ComboBox3.AddItem "10"
+    //ComboBox4.AddItem "0.4"
+    //ComboBox4.AddItem "0.6"
+    ////ComboBox4.AddItem "0.9"
+    //ComboBox5.AddItem "0.75"
+    //ComboBox5.AddItem "10"
+    //ComboBox8.AddItem "0.2"
+    //ComboBox8.AddItem "10"
+    //ComboBox9.AddItem "2.5"
+    //ComboBox10.AddItem "0.2"
+    //ComboBox10.AddItem "0.4"
+    //ComboBox10.AddItem "0.6"
+    //ComboBox10.AddItem "0.9"
+    //ComboBox11.AddItem "48.1"
+    //ComboBox11.AddItem "72.5"
+    //ComboBox11.AddItem "97.6"
+    //ComboBox12.AddItem "0.00334"
+    //ComboBox12.AddItem "0.15"
+//
+}
